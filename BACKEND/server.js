dbd7f6f482191a09486d3140f70320ba4240405a
Shashi Varshneya <shashikant9993@gmail.com>
2025-07-05 19:55:28 +0530

    added home route

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -21,5 +21,9 @@ app.use('/auth', require('./routes/auth'));
 
 app.use('/employees',emprouter );
 
+app.get('/',(req,res)=>{
+    res.status(200).send('this is the employee backend home route');
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
